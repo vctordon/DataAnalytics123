273927d6341a4bd28ead1b00ea812adbe66d6223
victoria ordonez <vordonez@my.yearupunited.org>
2026-04-14 13:19:04 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,2 +1,8 @@
 /*
 Sakila is a database used to collect information about a movie studio and company for use by the staff*/
+
+USE sakila;
+SELECT * FROM actor;  -- retrieved [200] records
+
+SELECT * FROM film; -- retrieved [1000] records
+
