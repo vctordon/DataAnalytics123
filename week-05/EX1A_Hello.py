bea4d6802b1bebcc5941d3ae4c0403af42a289f1
Victoria Ordonezgit config --global user.email vordonez@my.yearupunited.org <vordonez@my.yearupunited.org>
2026-05-04 15:11:00 -0400

    Completed HW 1A

--- week-05/EX1A_Hello.py
+++ week-05/EX1A_Hello.py
@@ -1,1 +1,29 @@
+# HW 1A 
 print ("Hello World")
+
+# -------------------
+
+# Creating variable 
+message = 'Hello World'
+print (message)
+
+# -------------------
+# Displaying dollars and cents 
+
+dollars = 3
+cents = .50
+print(dollars + cents)
+ 
+ # The amounts were added together but does not add the last 0 behind .50 
+
+# -------------------
+#Displaying dollars and cents 
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str + " " + c_str)
+
+#--------------------
