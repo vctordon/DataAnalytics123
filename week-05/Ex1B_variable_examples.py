00d32953a07310bd8e2a1878dcc4cd0c5f19e18e
Victoria Ordonez <vordonez@my.yearupunited.org>
2026-05-05 09:49:47 -0400

    Completed 1A HW

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,60 @@
+# Sample Variables:
+
+customer_id = 'Jdoe'
+customer_name = 'Jane Doe'
+Customer_gender = 'Female'
+Customer_dob = '05/20/1996'
+drivers_license = '123456789'
+apolicy_num = '987654321'
+
+# String Names: 
+
+str_1 = 'Customer ID:'
+str_2 = 'Customer Full Name:'
+str_3 = 'Gender:'
+str_4 = 'Date of Birth:'
+str_5 = 'License Number:'
+str_6 = 'Auto Policy:'
+
+# Running Code: 
+print('CUSTOMER INFORMATION:')
+print(str_1 + " " + customer_id)
+print(str_2 + " " + customer_name)
+print(str_3 + " " + Customer_gender)
+print(str_4 + " " + Customer_dob)
+print(str_5 + " " + drivers_license)
+print(str_6 + " " + apolicy_num)
+
+# -------------------------------
+
+# Variable assumptions: 
+# Some of the variables will be string and some will be integers.
+# I may have an error with the backslashes on the dob
+# May have to re do code and add new variables
+
+# -------------------------------
+
+# My Information: 
+
+first_name = 'Victoria'
+last_name = 'Ordonez'
+city = 'Charlotte'
+state = 'North Carolina'
+
+# String for info 
+
+str_7 = 'First Name:'
+str_8 = 'Last Name:'
+str_9 = 'City:'
+str_10 = 'State'
+
+# Running my info code
+print('MY PERSONAL INFORMATION:')
+print(str_7+ " " + first_name)
+print(str_8 + " " + last_name)
+print(str_9 + " " + city)
+print(str_10 + " " + state)
+
+# -------------------------------
+
+
