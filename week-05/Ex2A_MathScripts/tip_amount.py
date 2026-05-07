be15a16730ff9983f7fad5db152566f3be362dc1
Victoria Ordonez <vordonez@my.yearupunited.org>
2026-05-06 23:05:54 -0400

    lab 2 pt 3

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,15 @@
+#Figure out the formula and what the script would look like
+# # making up example values as needed. 
+# (If you need inspiration
+#  what was your approximate restaurant bill the last time you ate at a restaurant?)
+
+#The tip on a $[number] restaurant bill is $[number]
+#-----------------------------------------------------------------------------------
+#Calculating script
+bill_amount = 40.00
+tip_percent = .18
+
+#Calculate tip
+tip_amount = bill_amount * tip_percent
+
+print('The tip on a $',bill_amount,'bill is $',round(tip_amount,2))
