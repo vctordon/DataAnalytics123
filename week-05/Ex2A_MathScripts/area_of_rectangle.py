4fb6fa810f3b1b286df4d6eb6e2651d5bc0b6bf3
Victoria Ordonez <vordonez@my.yearupunited.org>
2026-05-06 22:45:54 -0400

    lab 2 hw

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,21 @@
+# Say you have a rectangle that has dimensions corresponding to your birthday – 
+# the month number is one side and the day of the month is the other side. 
+# How would you calculate the area of this rectangle?
+
+#Side A is [number]
+#Side B is [number]
+#The area of the rectangle is [number]
+
+#------------------------------------------------------------------------------
+
+#Rectangle Dimension
+sideA = 5 #Birthday month 
+sideB = 21 #Birthday day
+
+#Area Calculation
+Area = sideA * sideB
+
+print('Side A is',(sideA))
+print('Side B is',(sideB))
+print('The area of the rectangle is ',(Area))
+
